7465c334d42e7ccb0aae2d5d6263da7da651430e
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-23 17:14:04 +0800

    Final

--- exercise-10.py
+++ exercise-10.py
@@ -3,22 +3,22 @@ def new_list(list_1, list_2):
     # Empty list
     result_list = []
 
-# Iterate first list
-for i in list_1:
-    if i % 2 != 0:
-        result_list.append(i)
+    # Iterate first list
+    for i in list_1:
+        if i % 2 != 0:
+            result_list.append(i)
 
-# Iterate second list
-for i in list_2:
-    if i % 2 == 0:
-        result_list.append(i)
+    # Iterate second list
+    for i in list_2:
+        if i % 2 == 0:
+            result_list.append(i)
+    
     return result_list
 
 # Given
 list_1 = [23, 45, 86, 90, 13, 14]
-list_2 = [18, 76, 77, 95, 23, 45]
+list_2 = [18, 76, 77, 95, 24, 45]
 
-# Create new list based on condition
 result_list = new_list(list_1, list_2)
 
 # Print new list
